8f750c21db9b53a82d9500945f4d8cf438535205
pregen <pregen@kth.se>
2020-01-16 21:44:12 +0100

    ...

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var moment = require('moment')
 var myDate = new Date();
 var myCoolDate = moment(myDate).format('LL');
 
-console.log('old ' + myDate);
-console.log('new ' + myCoolDate);
+console.log("\x1b[41m", 'old ' + myDate);
+console.log("\x1b[0m", 'new ' + myCoolDate);
